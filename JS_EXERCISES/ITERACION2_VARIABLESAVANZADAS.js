deea961f086416a9b5393ec3e236474a3fa79d80
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-04-04 16:34:14 +0200

    Ejercicio 2

---
+++ JS_EXERCISES/ITERACION2_VARIABLESAVANZADAS.js
@@ -0,0 +1,15 @@
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
+    
+    const firstName = "Jon";
+    const lastName = "Snow";
+    const age = 24;
+
+    
+    let presentacion = "Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.";
+  
+    console.log(presentacion);

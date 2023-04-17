f4b66c007fbc5dbf01c5e98bd2be93e425be5869
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-04-17 20:53:11 +0200

    Ejercicios

--- JS_FUNCTIONS/ITERACIÓN1.js
+++ JS_FUNCTIONS/ITERACIÓN1.js
@@ -71,7 +71,7 @@ function averageWord(param) {
 /***Iteración #6: Valores únicos**
 
 /Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:*/
-const duplicates = [                  ////RESUELTO CONSULTANDO https://dev.to/soyleninjs/3-ways-to-remove-duplicates-in-an-array-in-javascript-259o ////
+const duplicates = [                  
   'sushi',
   'pizza',
   'burger',
@@ -86,17 +86,13 @@ const duplicates = [                  ////RESUELTO CONSULTANDO https://dev.to/so
 ];
 
 
-let removeDuplicates = duplicates.filter((element, index) => {   //Explicación de dev.to: To eliminate duplicates, the filter() method is used to include only the elements whose indexes match their indexOf values, since we know that the filer method returns a new array based on the operations performed on it:
-    return duplicates.indexOf(element) === index;
-});
 
-console.log(removeDuplicates);
 
 /***Iteración #7: Buscador de nombres**
 
 Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:*/
 
-const nameFinder = [                            ////RESUELTO CONSULTANDO https://stackoverflow.com/questions/27541311/search-a-word-in-array-of-words
+const nameFinder = [                            
   'Peter',
   'Steve',
   'Tony',
@@ -109,12 +105,12 @@ const nameFinder = [                            ////RESUELTO CONSULTANDO https:/
   'Jessica',
   'Marc'
 ];
-function search (term) {
-  for (var i = 0, len = nameFinder.length; i < len; i++) {
-    if (nameFinder[i] === term) {
-      console.log(nameFinder[i] + ' is found at ' + i);
-    }
-  }
+
+function buscadorNombre (nombre, array) {
+
+  if (array.includes(nombre));
+  console.log (("Nombre Incluido") && buscadorNombre.indexOf()); 
+  
 }
 
-search('Natasha');
+buscadorNombre("Natasha", nameFinder);

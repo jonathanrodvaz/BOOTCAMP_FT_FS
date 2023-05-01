24c6103bc1336af20697168cb3234cc194096836
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-05-01 20:45:52 +0200

    Ejercicios DOM WIP

--- JS_EXERCISES/JS_DOM/appIteracionDos.js
+++ JS_EXERCISES/JS_DOM/appIteracionDos.js
@@ -10,13 +10,38 @@ const template = `
 document.body.innerHTML = template;
 
 //2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+const dynamicDiv = () => {
+    const divscontainer = document.querySelector(".div");
+    for (let i = 1; i <= 6; i++) {
+        divscontainer.innerHTML += `<p class="parrafo">${i}</>`
+    }
+}
 
 //2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+const soyDinamico = () => {
+    return`
+    <p>Soy dinámico!.</p>
+    `
+}
 
 //2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+const wubLubDub = () => {
+    const wubwub = document.querySelector("#.fn-insert-here");
+    wubwub.innerHTML = `Wubba Lubba dub dub`
+}
 
 //2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
-//const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const listaApps = () => {
+    for (const app of apps) {
+       listaApps.innerHTML += `
+       <ul>
+        <li>${app}</li>
+       </ul>`
+    }
+}
+
+
 
 //2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
 
